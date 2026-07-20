db77a8ffa9614007757a02f7cbf1b7160b2f4a17
himanshu-antal <himanshuantal26@gmail.com>
2026-07-20 12:11:49 +0530

    fixing ui sytle

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   server: {
     port: 5173,
     proxy: {
@@ -15,4 +19,3 @@ export default defineConfig({
     },
   },
 })
-
